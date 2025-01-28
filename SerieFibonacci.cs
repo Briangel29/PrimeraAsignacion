a1eedc524131e4dddbd502194b82f2217de44511
Briangel Ismael Abreu Hernandez <briangel.abreu@inabima.gob.do>
2025-01-28 15:34:25 -0400

    Implementacion de la clase SerieFibonacci

--- SerieFibonacci.cs
+++ SerieFibonacci.cs
@@ -9,16 +9,22 @@ namespace PrimeraAsignacion
     public class SerieFibonacci
     {
         const int limite = 50;
-        private int sumado = 1;
+        private int variableY = 0;
+        private int variableX = 1;
         private List<int> numeros = new List<int>();
 
         public List<int> NumerosFibonacci()
         {
-
             for (int inicio = 0; inicio < limite; inicio++)
             {
-                numeros.Add(inicio * sumado);
-                sumado = inicio;
+                numeros.Add(variableX + variableY);
+                variableY = variableX;
+                variableX = numeros[inicio];
+
+                if (numeros[inicio] >=50)
+                {
+                    break;
+                }
             }
 
             return numeros;
